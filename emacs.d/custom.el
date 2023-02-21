d7b9109d74cfc0a31658b6006c6881ccc3dc6408
Luis E. Fernandez de la Vara <luis@lefv.info>
2023-02-21 01:07:15 -0500

    Working mutt email files.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,10 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(cua-mode t)
- '(custom-enabled-themes '(light-blue))
+ '(custom-enabled-themes '(misterioso modus-vivendi light-blue))
  '(display-battery-mode t)
  '(display-time-mode t)
  '(font-use-system-font t)
+ '(global-display-line-numbers-mode t)
  '(notmuch-saved-searches
    '((:name "inbox" :query "tag:inbox" :key "i" :sort-order newest-first)
      (:name "unread" :query "tag:unread" :key "u" :sort-order newest-first)
