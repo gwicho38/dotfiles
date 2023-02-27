94af1b28bd4da2fdf102d9137b9491a8c2f0333f
Luis E. Fernandez de la Vara <luis@lefv.info>
2023-02-27 16:56:06 -0500

    Saving lefv emacs config.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,26 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(cua-mode t)
- '(custom-enabled-themes '(misterioso modus-vivendi light-blue))
- '(display-battery-mode t)
- '(display-time-mode t)
- '(font-use-system-font t)
- '(global-display-line-numbers-mode t)
- '(notmuch-saved-searches
-   '((:name "inbox" :query "tag:inbox" :key "i" :sort-order newest-first)
-     (:name "unread" :query "tag:unread" :key "u" :sort-order newest-first)
-     (:name "flagged" :query "tag:flagged" :key "f" :sort-order newest-first)
-     (:name "sent" :query "tag:sent" :key "t" :sort-order newest-first)
-     (:name "drafts" :query "tag:draft" :key "d" :sort-order newest-first)
-     (:name "all mail" :query "*" :key "a" :sort-order newest-first)))
  '(package-selected-packages '(use-package))
- '(save-place-mode t)
- '(send-mail-function 'smtpmail-send-it)
- '(smtpmail-smtp-server "127.0.0.1")
- '(smtpmail-smtp-service 1025)
- '(tab-bar-mode t))
+ '(warning-suppress-types
+   '((comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
